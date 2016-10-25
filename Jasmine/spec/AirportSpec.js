9536d8b30cbd6bace76e26658fdea93f0c06901b
Laurent Bouhnik <misterbl@hotmail.com>
2016-10-25 16:52:14 +0100

    Aiport challenge finished

--- Jasmine/spec/AirportSpec.js
+++ Jasmine/spec/AirportSpec.js
@@ -3,19 +3,20 @@ describe('Airport', function () {
     var plane = new Plane ();
     var plane2 = new Plane ();
     var plane3 = new Plane ();
+
   it ('has a limited capicity of planes', function () {
     expect(airport.capacity).toEqual(2);
   });
 
-
   it ('adds landing plane to the list of landed plane', function () {
+      spyOn(airport, 'isBadWeather').and.returnValue(false)
       airport.landedPlane(plane)
       expect(airport.listLandedPlanes).toContain(plane)
   });
 
 
   it ('confirms a plane has landed', function (){
-    expect(airport.confirmsLanded(plane)).toEqual("The plane has landed")
+    expect(airport.confirmsLanded(plane)).toEqual('The plane has landed')
   });
 
   it ('removes taking off plane from the list of landed plane', function () {
@@ -24,29 +25,24 @@ describe('Airport', function () {
   });
 
   it ('confirms a plane has taken off', function (){
-    expect(airport.confirmsTakeOff(plane)).toEqual("The plane has taken off")
+    expect(airport.confirmsTakeOff(plane)).toEqual('The plane has taken off')
   })
 
   it ('doesn\'t let a plane to land if it is full', function () {
-    airport.landedPlane(plane)
-    airport.landedPlane(plane2)
-    airport.landedPlane(plane3)
-(    expect(airport.fullCapacity()).toEqual("The airport is full, the plane can't land")
-)  });
+    spyOn(airport, 'isBadWeather').and.returnValue(false)
+    airport.landedPlane(plane);
+    airport.landedPlane(plane2);
+    airport.landedPlane(plane3);
+    expect(function () {airport.fullCapacity()}).toThrow('The airport is full, the plane can\'t land')
+  });
+
+  it ('forbids planes to land if the weather is stormy', function () {
+    spyOn(airport, 'isBadWeather').and.returnValue(true)
+    expect(function () {airport.landedPlane(plane)}).toThrow('The plane can\'t land due to stormy weather');
+  });
+
+  it ('forbids planes to take off if the weather is stormy', function () {
+    spyOn(airport, 'isBadWeather').and.returnValue(true)
+    expect(function () {airport.takenOffPlane(plane)}).toThrow('The plane can\'t take off due to stormy weather');
+  });
 });
-//
-//   it "forbids planes to land if the weather is stormy" do
-//   plane = double(:plane)
-//   allow(plane).to receive(:plane_number).and_return('1234')
-//   allow(subject).to receive(:stormy) {true}
-//   expect(subject.landed_planes(plane)).to eq("The plane #{plane.plane_number} can't land due to stormy weather")
-//   end
-//
-//   it "forbids planes to take off if the weather is stormy" do
-//   plane = double(:plane)
-//   allow(plane).to receive(:plane_number).and_return('1234')
-//   allow(subject).to receive(:stormy) {true}
-//   expect(subject.taken_off_planes(plane)).to eq("The plane #{plane.plane_number} can't take off due to stormy weather")
-//   end
-//
-// end
--- Jasmine/src/Airport.js
+++ Jasmine/src/Airport.js
@@ -1,28 +1,45 @@
 function Airport () {
   this.capacity = 2
   this.listLandedPlanes = []
+  this.weather = new Weather ()
 };
+
 
 
 Airport.prototype.landedPlane = function (plane) {
-  this.listLandedPlanes.push(plane)
-  plane.landPlane ()
+  if(this.isBadWeather () === true){
+    throw ('The plane can\'t land due to stormy weather');
+  }
+  else {
+    this.listLandedPlanes.push(plane)
+    plane.landPlane ()
+  }
 };
 
 Airport.prototype.confirmsLanded = function (plane) {
-  return "The plane has landed"
+  return 'The plane has landed'
 };
 
 Airport.prototype.takenOffPlane = function (plane) {
+  if(this.isBadWeather () === true){
+    throw ('The plane can\'t take off due to stormy weather');
+  }
+  else {
   this.listLandedPlanes.splice(this.listLandedPlanes.indexOf(plane), 1 )
   plane.takesOff ()
+}
 };
 
 Airport.prototype.confirmsTakeOff = function (plane) {
-  return "The plane has taken off"
+  return 'The plane has taken off'
 };
 
-// Airport.prototype.fullCapacity = function () {
-//   if this.listLandedPlanes.length >= this.capacity
-//   return "The airport is full, the plane can't land"
-// }
+Airport.prototype.fullCapacity = function () {
+  if (this.listLandedPlanes.length >= this.capacity) {
+    throw ('The airport is full, the plane can\'t land')
+  };
+};
+
+Airport.prototype.isBadWeather = function () {
+  return this.weather.isStormy
+}
